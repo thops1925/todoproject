7734b87fae7d569045b0c0ce123f9eaa1f27505d
thops1025 <clumen600@gmail.com>
2021-08-21 21:59:37 +0800

    right

--- src/pages/List.js
+++ src/pages/List.js
@@ -30,7 +30,7 @@ export default function List() {
     return (
         <div className="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8 ">
             <div className="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
-                <table className="min-w-full divide-y divide-gray-200">
+                <table className="w-screen divide-y divide-gray-200 ">
                     <thead className="bg-gray-50">
                         <tr>
                             <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
